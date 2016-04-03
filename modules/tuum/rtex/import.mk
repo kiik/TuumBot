82976c7fa46a1927b27164d52d9516d74aa07048
Meelik Kiik <kiik.meelik@gmail.com>
2016-04-03 04:26:07 +0300

    Minor build flags update.

--- modules/tuum/rtex/import.mk
+++ modules/tuum/rtex/import.mk
@@ -1,3 +1,3 @@
 RTEX_MODULE_VERSION ?= 0
-RTEX_MODULE_PATH ?= $(PROJECT_ROOT)/modules/phobos/rtex
+RTEX_MODULE_PATH ?= $(PROJECT_ROOT)/modules/rtex
 include $(call rwildcard,$(RTEX_MODULE_PATH)/,include.mk)
